a249a7d02fbcabc0851b1af6c68a411d475dd439
emkeris <emkeris@gmail.com>
2019-04-27 21:57:25 +0100

    landing page and footer done(no animation)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderBottomShortCutComponent } from './header/header-bottom-short-cut/header-bottom-short-cut.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    HeaderBottomShortCutComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header-bottom-short-cut/header-bottom-short-cut.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-bottom-short-cut',
+  templateUrl: './header-bottom-short-cut.component.html',
+  styleUrls: ['./header-bottom-short-cut.component.scss']
+})
+export class HeaderBottomShortCutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
